1bfaa1e00a096c670460f04551fd4472e112cff0
Ewan May <ewan.may@arcurve.com>
2020-07-16 20:43:49 -0600

    progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,8 @@
 import React from 'react';
-import { Transition } from 'react-transition-group';
 import './App.css';
 import { Contact, Landing, Navbar, Projects, Timeline, Values } from './components';
 
 function App() {
-  const duration = 300;
-
-  const defaultStyle = {
-    transition: `opacity ${duration}ms ease-in-out`,
-    opacity: 0,
-  }
-
-  const transitionStyles: any = {
-    entering: { opacity: 1 },
-    entered: { opacity: 1 },
-    exiting: { opacity: 0 },
-    exited: { opacity: 0 },
-  };
-
-  const Fade = ({ in: inProp }: any) => {
-    return (
-      <Transition in={inProp} timeout={duration}>
-        {(state: any) => (
-          <div style={{
-            ...defaultStyle,
-            ...transitionStyles[state]
-          }}>
-            I'm a fade Transition!
-          </div>
-        )}
-      </Transition>
-    );
-  };
 
   return (
     <div className="App">
