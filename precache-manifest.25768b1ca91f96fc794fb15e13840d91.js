a6b01ce463f5fbc10ffa4e66ca27acfd0c33ffa7
Ewan May <ewan.may@arcurve.com>
2020-07-16 21:17:22 -0600

    Updates

---
+++ precache-manifest.25768b1ca91f96fc794fb15e13840d91.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa8cdf4147ee9b59d01db751a3853f74",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8adb572d40e27cb81e7a",
+    "url": "/static/css/main.cecb7861.chunk.css"
+  },
+  {
+    "revision": "2e533d9e5449ce19a81b",
+    "url": "/static/js/2.d05d3aac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d05d3aac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8adb572d40e27cb81e7a",
+    "url": "/static/js/main.67521f36.chunk.js"
+  },
+  {
+    "revision": "ba126735810163618457",
+    "url": "/static/js/runtime-main.3374ac37.js"
+  },
+  {
+    "revision": "c03c2804bb8dbbd309bbd07aae06c776",
+    "url": "/static/media/ArcurveIcon.c03c2804.svg"
+  },
+  {
+    "revision": "e78235ed747204d1cf9d0cc484818f77",
+    "url": "/static/media/CloudPantry.e78235ed.svg"
+  },
+  {
+    "revision": "1e3853d8cc77bd556c79af776120b8bd",
+    "url": "/static/media/ESSLogo.1e3853d8.svg"
+  },
+  {
+    "revision": "3d935f94cd09e8c2780257f261cb480e",
+    "url": "/static/media/GarageDoor.3d935f94.svg"
+  },
+  {
+    "revision": "329c7bf8c129fef669f18fe06349a633",
+    "url": "/static/media/GithubIcon.329c7bf8.svg"
+  },
+  {
+    "revision": "9433b52393f0eb2bfde6450f1cf7ad27",
+    "url": "/static/media/LandingPhoto.9433b523.svg"
+  },
+  {
+    "revision": "8efc590367e16d813dc02fb84abf1df2",
+    "url": "/static/media/ProjectIcon.8efc5903.svg"
+  },
+  {
+    "revision": "9a3ba20611768b6ad7e300b5d0261554",
+    "url": "/static/media/RepliconLogo.9a3ba206.svg"
+  },
+  {
+    "revision": "ada6f53c146217e35631a05a6546bcdd",
+    "url": "/static/media/SeroTrackerIcon.ada6f53c.svg"
+  },
+  {
+    "revision": "f6a1bac296ea46c3266f1d904a3146c2",
+    "url": "/static/media/TicTracker.f6a1bac2.svg"
+  },
+  {
+    "revision": "545a11e8895a0f176a42defa218946a4",
+    "url": "/static/media/TimelineIcon.545a11e8.svg"
+  },
+  {
+    "revision": "045d7697b1d3356785bbf6023cb215d0",
+    "url": "/static/media/UniversityOfCalgaryLogo.045d7697.svg"
+  },
+  {
+    "revision": "6f168a340305481a7abaaf419472d9a2",
+    "url": "/static/media/WonderLivesHere.6f168a34.svg"
+  },
+  {
+    "revision": "5a513ab32230bbb84ed56fbf533f4f2c",
+    "url": "/static/media/email.5a513ab3.svg"
+  },
+  {
+    "revision": "a08a395dd61b7a37d52fec5b5ae79d9e",
+    "url": "/static/media/linked-in.a08a395d.svg"
+  }
+]);
